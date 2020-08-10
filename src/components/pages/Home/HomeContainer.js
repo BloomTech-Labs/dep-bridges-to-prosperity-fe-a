cd9ef6fb953d5d34f58b8ff64678abcbcab91241
Pinefoot <jameslcarpino@gmail.com>
2020-08-10 14:22:12 -0700

    adjusted title - removed placeholder.

--- src/components/pages/Home/HomeContainer.js
+++ src/components/pages/Home/HomeContainer.js
@@ -9,8 +9,9 @@ function HomeContainer() {
 
   return (
     <div>
-      <h1>Home page redux + hooks example</h1>
+      <h1>Bridges To Prosperity</h1>
       <Mapbox />
+      {/* not sure if I should remove this or not? */}
       <div className="counter">
         <button onClick={() => dispatch(decrement())}>Decrement</button>
         <div className="count">{count}</div>
