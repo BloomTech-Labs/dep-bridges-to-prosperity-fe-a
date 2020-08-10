3359d86f169cf41ff0dd126c0e4bee147ddcbbfa
Pinefoot <jameslcarpino@gmail.com>
2020-08-10 13:21:29 -0700

    incorporated mapbox component into home page

--- src/components/pages/Home/HomeContainer.js
+++ src/components/pages/Home/HomeContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from '../../../state/actions';
+import Mapbox from './Mapbox';
 
 function HomeContainer() {
   const dispatch = useDispatch();
@@ -9,6 +10,7 @@ function HomeContainer() {
   return (
     <div>
       <h1>Home page redux + hooks example</h1>
+      <Mapbox />
       <div className="counter">
         <button onClick={() => dispatch(decrement())}>Decrement</button>
         <div className="count">{count}</div>
