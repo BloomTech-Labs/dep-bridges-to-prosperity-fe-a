8daecf5dc185ff838949d3d086527ce10e0930be
Pinefoot <jameslcarpino@gmail.com>
2020-08-11 14:52:52 -0700

    Menu now has map in its content layout.

--- src/components/pages/Home/MapMenu.js
+++ src/components/pages/Home/MapMenu.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 //antDesign
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Input } from 'antd';
+import MapSearchBar from './MapSearchBar';
+import Mapbox from './Mapbox';
 
 function MapMenu() {
-  const { Header, Content, Footer, Sider } = Layout;
-  const { SubMenu } = Menu;
+  const { Sider, Content } = Layout;
+  //   const { SubMenu } = Menu;
+
   const [slide, setSlide] = useState({
     collapsed: false,
   });
@@ -15,7 +18,23 @@ function MapMenu() {
     };
   };
 
-  return <div></div>;
+  return (
+    <div>
+      <Layout theme="dark" style={{ minHeight: '100%' }}>
+        <Sider collapsible collapsed={slide} onCollapse={setSlide}>
+          <Menu mode="inline">
+            <Menu.Item>
+              <MapSearchBar />
+            </Menu.Item>
+
+            {/* <Menu.Item key="1">Bridge Sites</Menu.Item>
+            <Menu.Item key="2">Village Sites</Menu.Item> */}
+          </Menu>
+        </Sider>
+        <Mapbox />
+      </Layout>
+    </div>
+  );
 }
 
 export default MapMenu;
