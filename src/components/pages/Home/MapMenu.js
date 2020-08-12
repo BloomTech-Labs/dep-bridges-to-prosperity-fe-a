bf644b13a1f8031602c19812444b76ecdbfb8184
Pinefoot <jameslcarpino@gmail.com>
2020-08-12 11:51:43 -0700

    Incorporated drawer for info display

--- src/components/pages/Home/MapMenu.js
+++ src/components/pages/Home/MapMenu.js
@@ -3,19 +3,69 @@ import React, { useState } from 'react';
 import { Layout, Menu, Breadcrumb, Input } from 'antd';
 import MapSearchBar from './MapSearchBar';
 import Mapbox from './Mapbox';
+import InfoDrawer from './InfoDrawer';
 
-function MapMenu() {
+//DUMMY DATA TO BE DELETED:
+let mapData = [
+  {
+    id: 1,
+    site_name: 'Buzi',
+    proj_stage: 'rejected',
+    latitude: -2.42056,
+    longitude: 28.9662,
+  },
+  {
+    id: 2,
+    site_name: 'Kamigisha',
+    proj_stage: 'rejected',
+    latitude: -2.42486,
+    longitude: 28.95726,
+  },
+  {
+    id: 3,
+    site_name: 'Nyarubande',
+    proj_stage: 'rejected',
+    latitude: -1.65595,
+    longitude: 30.07884,
+  },
+];
+
+function MapMenu(props) {
   const { Sider } = Layout;
   //   const { SubMenu } = Menu;
 
   const [slide, setSlide] = useState({
     collapsed: false,
   });
+
+  //state for information to be displayed
+  const [infoDisplay, setInfoDisplay] = useState({
+    site_name: '',
+    proj_stage: '',
+    latitude: null,
+    longitude: null,
+  });
+
+  const [clickedBridge, setClickedBridge] = useState([]);
+
   //handles the collapse feature of the sidebar
   const onCollapse = () => {
-    setSlide = {
+    setSlide({
       collapsed: true,
-    };
+    });
+  };
+
+  //handles the click feature of the info
+  const clickMarker = bridge => {
+    setInfoDisplay({
+      site_name: bridge.site_name,
+      proj_stage: bridge.proj_stage,
+      latitude: bridge.latitude,
+      longitude: bridge.longitude,
+    });
+    setClickedBridge(bridge);
+    console.log('clickMarker');
+    console.log(bridge);
   };
 
   return (
@@ -34,6 +84,7 @@ function MapMenu() {
             <Menu.Item>
               <MapSearchBar />
             </Menu.Item>
+
             {/* When these Items are toggled, only display that which is selected*/}
             {/* Leaving this here for now, as an option -> going to mostlikely delete today for rendering bridge info */}
             {/* <Menu.Item key="1">All Markers</Menu.Item>
@@ -44,7 +95,7 @@ function MapMenu() {
             <Menu.Item key="6">Rejected Bridge Sites</Menu.Item> */}
           </Menu>
         </Sider>
-        <Mapbox />
+        <Mapbox clickMarker={clickMarker} />
       </Layout>
     </div>
   );
