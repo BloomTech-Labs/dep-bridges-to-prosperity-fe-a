b108cfcc57a50be075797ccfe6405b39217da989
Pinefoot <jameslcarpino@gmail.com>
2020-08-12 14:08:53 -0700

    cleaned up some code

--- src/components/pages/Home/MapMenu.js
+++ src/components/pages/Home/MapMenu.js
@@ -1,42 +1,12 @@
 import React, { useState } from 'react';
 //antDesign
-import { Layout, Menu, Breadcrumb, Input } from 'antd';
+import { Layout, Menu } from 'antd';
 import MapSearchBar from './MapSearchBar';
 import Mapbox from './Mapbox';
 import InfoDrawer from './InfoDrawer';
 
-//DUMMY DATA TO BE DELETED:
-let mapData = [
-  {
-    id: 1,
-    site_name: 'Buzi',
-    proj_stage: 'rejected',
-    latitude: -2.42056,
-    longitude: 28.9662,
-  },
-  {
-    id: 2,
-    site_name: 'Kamigisha',
-    proj_stage: 'rejected',
-    latitude: -2.42486,
-    longitude: 28.95726,
-  },
-  {
-    id: 3,
-    site_name: 'Nyarubande',
-    proj_stage: 'rejected',
-    latitude: -1.65595,
-    longitude: 30.07884,
-  },
-];
-
 function MapMenu(props) {
   const { Sider } = Layout;
-  //   const { SubMenu } = Menu;
-
-  const [slide, setSlide] = useState({
-    collapsed: false,
-  });
 
   //state for information to be displayed
   const [infoDisplay, setInfoDisplay] = useState({
@@ -48,12 +18,6 @@ function MapMenu(props) {
 
   const [clickedBridge, setClickedBridge] = useState({});
   const [visible, setVisible] = useState(false);
-  //handles the collapse feature of the sidebar
-  const onCollapse = () => {
-    setSlide({
-      collapsed: true,
-    });
-  };
 
   //handles the click feature of the info
   const clickMarker = bridge => {
@@ -74,6 +38,11 @@ function MapMenu(props) {
 
   return (
     <div>
+      <InfoDrawer
+        clickedBridge={clickedBridge}
+        visible={visible}
+        onClose={onClose}
+      ></InfoDrawer>
       <Layout
         theme="dark"
         style={{
@@ -88,11 +57,7 @@ function MapMenu(props) {
             <Menu.Item>
               <MapSearchBar />
             </Menu.Item>
-            <InfoDrawer
-              clickedBridge={clickedBridge}
-              visible={visible}
-              onClose={onClose}
-            ></InfoDrawer>
+
             {/* When these Items are toggled, only display that which is selected*/}
             {/* Leaving this here for now, as an option -> going to mostlikely delete today for rendering bridge info */}
             {/* <Menu.Item key="1">All Markers</Menu.Item>
