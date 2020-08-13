060b08440e4e7a610911643d26bef7d143b991a2
GeraldRyan <gerald.ryan40@yahoo.com>
2020-08-13 15:36:06 -0500

    moved dummy data to separate file

--- src/components/pages/Home/Mapbox.js
+++ src/components/pages/Home/Mapbox.js
@@ -3,31 +3,7 @@ import ReactMapGl, { Marker, FlyToInterpolator } from 'react-map-gl';
 import { UpCircleFilled } from '@ant-design/icons';
 import { Tooltip } from 'antd';
 import InfoDrawer from './InfoDrawer';
-
-//DUMMY DATA TO BE DELETED:
-let mapData = [
-  {
-    id: 1,
-    site_name: 'Buzi',
-    proj_stage: 'rejected',
-    latitude: -2.42056,
-    longitude: 28.9662,
-  },
-  {
-    id: 2,
-    site_name: 'Kamigisha',
-    proj_stage: 'rejected',
-    latitude: -2.42486,
-    longitude: 28.95726,
-  },
-  {
-    id: 3,
-    site_name: 'Nyarubande',
-    proj_stage: 'rejected',
-    latitude: -1.65595,
-    longitude: 30.07884,
-  },
-];
+import { mapData } from './dummyData';
 
 function Mapbox(props) {
   //copy code from previous proj.
