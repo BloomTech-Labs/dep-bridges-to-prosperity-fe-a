029a9ded5b03feafe3b4d3655c27dd4ca5768f31
Pinefoot <jameslcarpino@gmail.com>
2020-08-17 13:28:05 -0700

    cleaned up code

--- src/components/pages/Home/InfoDrawer.js
+++ src/components/pages/Home/InfoDrawer.js
@@ -2,21 +2,12 @@ import React, { useEffect } from 'react';
 import { Drawer } from 'antd';
 
 const InfoDrawer = props => {
-  //   const [visible, setVisible] = useState(false);
-
-  //     const showDrawer = () => {
-  //       setVisible(true);
-  //     };
-
   useEffect(() => {
     console.log('infodrawer line 14', props.clickedBridge);
   }, [props.clickedBridge]);
 
   return (
     <>
-      {/* <Button type="primary" onClick={showDrawer}>
-        Open
-      </Button> */}
       <Drawer
         title="Bridge Information"
         placement="left"
