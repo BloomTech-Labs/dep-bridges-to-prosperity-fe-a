fa08216928bc20cd3540b933ea683767423f785c
Pinefoot <jameslcarpino@gmail.com>
2020-08-25 12:38:46 -0700

    mapbox commit before pull

--- src/components/pages/Home/Mapbox.js
+++ src/components/pages/Home/Mapbox.js
@@ -28,7 +28,7 @@ function Mapbox({
           setViewport(viewport);
         }}
         //using minimo right now, should be good to swap out if we want another
-        setStyle={setTheme}
+        mapStyle={theme.mapStyle}
       >
         {/* maps the points of the data to the map: bridges, villiages, etc. */}
         {/* currently just showing bridge lats/longs */}
