282401dae049705550aad4f581cec181b0853f22
GeraldRyan <gerald.ryan40@yahoo.com>
2020-08-25 11:48:59 -0500

    created landing page

---
+++ src/components/pages/LandingPage/LandingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.less';
+
+// Here is an example of using our reusable List component to display some list data to the UI.
+const LandingPage = () => {
+  return <div className="landing-page-wrapper">Hello World</div>;
+};
+
+export default LandingPage;
