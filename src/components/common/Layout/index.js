49558f8a30ec67be1f7b1fb2667a591d0eaa6e8a
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-09-17 17:02:51 -0500

    Sign in and sign out functionality set up for nav

--- src/components/common/Layout/index.js
+++ src/components/common/Layout/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useOktaAuth } from '@okta/okta-react';
 import './styles.less';
 import logo from './logo.png';
 import { ShowExplorerContext } from '../../../state/context/showExplorer';
@@ -25,6 +26,23 @@ export default function Layout({ children, onClear, toggleBridges }) {
       }));
     }
   };
+
+  /******* TO SIGN OUT *******/
+  const { authState, authService } = useOktaAuth();
+
+  const issuer = 'https://auth.lambdalabs.dev/oauth2/default';
+  const redirectUri = `${window.location.origin}/`;
+
+  const logout = async () => {
+    // Reads the idToken before local session is cleared
+    window.localStorage.removeItem('bridge');
+    const idToken = authState.idToken;
+    await authService.logout('/');
+
+    // Clears the remote session
+    window.location.href = `${issuer}/v1/logout?id_token_hint=${idToken}&post_logout_redirect_uri=${redirectUri}`;
+  };
+
   return (
     <div className="layout-wrapper">
       <nav className="navbar">
@@ -36,17 +54,17 @@ export default function Layout({ children, onClear, toggleBridges }) {
           <button className="nav-buttons b-e" onClick={showMap}>
             Map
           </button>
-          <button
-            className="about nav-buttons"
-            onClick={() =>
-              setContextState(contextState => ({
-                ...contextState,
-                show: 'landing-page-wrapper-hidden',
-              }))
-            }
-          >
-            Sign In
-          </button>
+          {authState.idToken ? (
+            <button className="about nav-buttons" onClick={logout}>
+              Sign Out
+            </button>
+          ) : (
+            <button className="about nav-buttons">
+              <a href="/login" style={{ color: 'black' }}>
+                Sign In
+              </a>
+            </button>
+          )}
         </div>
       </nav>
       <div className="spacer" />
